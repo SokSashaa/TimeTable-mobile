70e94c658df1932b02b0266a0eef1e4e12a6ea44
SokSashaa <sashasokolov250603@mail.ru>
2023-03-30 21:06:24 +0300

    addLayouts

--- app/src/main/java/com/example/lab8/MainActivity.java
+++ app/src/main/java/com/example/lab8/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void Send(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.buttonGroupe:
+                Intent intent = new Intent(MainActivity.this,choosing_institute.class);
+                startActivity(intent);
+                break;
+            case R.id.buttonTeacher:
+                Intent intent1 = new Intent(MainActivity.this,choosing_teachers_sername.class);
+                startActivity(intent1);
+                break;
+        }
+    }
+
 }
