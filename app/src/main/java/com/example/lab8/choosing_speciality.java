366abc35f045f7bbc812509d462192d41ac685a5
SokSashaa <sashasokolov250603@mail.ru>
2023-03-31 00:48:44 +0300

    addSpecialityActivityAndClass

---
+++ app/src/main/java/com/example/lab8/choosing_speciality.java
@@ -0,0 +1,12 @@
+package com.example.lab8;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class choosing_speciality extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choosing_institute);
+    }
+}
