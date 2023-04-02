12a513f9e7f297c7aa3565185ca01bf278f533fb
SokSashaa <sashasokolov250603@mail.ru>
2023-04-03 01:32:02 +0300

    updateDBHelper

--- app/src/main/java/com/example/lab8/DBHelper.java
+++ app/src/main/java/com/example/lab8/DBHelper.java
@@ -9,16 +9,23 @@ import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION=2;
+    public static final int DATABASE_VERSION=1;
     public static final String DATABASE_NAME = "contract";
     public static final String TABLE_CONTACT="contacts";
 
     public static  final String KEY_ID="_id";
-    public static final String KEY_NAME="name";
-    public static final String KEY_DESCRIBTION="describtion";
-    public static final String KEY_DATE = "date";
-    public static final String KEY_TIME="time";
-    public static final String KEY_PRIORITY="priority";
+    public static final String KEY_INSTITUTE="inst";
+    public static final String KEY_SPEC="spec";
+    public static final String KEY_GROUP="groupe";
+    public static final String KEY_TYPE_WEEK="type_week";
+    public static final String KEY_DAY_OF_WEEK = "day_of_week";
+    public static final String KEY_SUBJECT = "subject";
+    public static final String KEY_TEACHER = "teacher";
+    public static final String KEY_PLACE = "place";
+    public static final String KEY_COMMENT = "comment";
+    public static final String KEY_TYPE_SUBJECT = "type_subject";
+    public static final String KEY_TIME = "time";
+
 
 
     public DBHelper(@Nullable Context context) {
@@ -27,10 +34,20 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_CONTACT + " (" + KEY_ID + " integer primary key,"+KEY_NAME+ " text," +KEY_DESCRIBTION + " text,"+
-                KEY_DATE + " text," + KEY_TIME + " text,"+KEY_PRIORITY + " integer" + ") ");
-
-
+        db.execSQL("create table " + TABLE_CONTACT + " ("
+                + KEY_ID + " integer primary key,"
+                +KEY_INSTITUTE+ " text,"
+                + KEY_SPEC + " text,"
+                + KEY_GROUP + " text,"
+                + KEY_TYPE_WEEK + " text,"
+                + KEY_DAY_OF_WEEK + " text,"
+                + KEY_TIME + " text,"
+                + KEY_SUBJECT + " text,"
+                + KEY_PLACE + " text,"
+                + KEY_TYPE_SUBJECT + " text,"
+                + KEY_TEACHER + " text,"
+                + KEY_COMMENT + " text"
+                + ") ");
     }
 
     @Override
