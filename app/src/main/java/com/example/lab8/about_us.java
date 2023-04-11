65e206f04001704d5ed3b7eb99bd5e5952e5d130
Кирилл <kiryann888@gmail.com>
2023-04-11 23:10:14 +0300

    ABOUTUS

--- app/src/main/java/com/example/lab8/about_us.java
+++ app/src/main/java/com/example/lab8/about_us.java
@@ -1,7 +1,33 @@
 package com.example.lab8;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class about_us extends AppCompatActivity {
+import java.net.URL;
 
+public class about_us extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_us);
+
+    }
+    public void Send(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.button:
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/soksasha"));
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/slepojboy"));
+                startActivity(intent1);
+                break;
+        }
+    }
 }
